ac7e23ca15b736abe967708b925381bffcad15de
MrQwest <128bit@128bit.xyz>
2025-05-19 22:23:39 +0300

    fix: add service tokens for transformer

--- tokenizer/module.py
+++ tokenizer/module.py
@@ -3,14 +3,14 @@ import json
 class Tokenizer:
     def __init__(self,):
         self.vocab = {}
-        enc = {}
-        dec = {}
+        enc = {'<SOS>': 0, '<EOS>': 1, '<UNK>': 2, '<SOC>': 3, '<EOC>': 4}
+        dec = {0: '<SOS>', 1: '<EOS>', 2: '<UNK>', 3: '<SOC>', 4: '<EOC>'}
         hex_list = [f"{i:04x}" for i in range(0x10000)]
         for i in range(len(hex_list)):
-            enc[hex_list[i]] = i
-            dec[i] = hex_list[i]
-            self.vocab["enc"] = enc
-            self.vocab["dec"] = dec
+            enc[hex_list[i]] = i + 5
+            dec[i + 5] = hex_list[i]
+        self.vocab["enc"] = enc
+        self.vocab["dec"] = dec
 
     def read_hex_chunks(self, file_path, chunk_size=2, buffer_size=4096):
         with open(file_path, 'rb') as f:
