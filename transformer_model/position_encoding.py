d7084194c1f0476f3650aab32f247a6896826509
MrQwest <128bit@128bit.xyz>
2025-05-19 15:26:46 +0300

    feat: add position encoding

---
+++ transformer_model/position_encoding.py
@@ -0,0 +1,25 @@
+import torch
+import torch.nn as nn
+import math
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model:int, seq_len:int, dropout:float):
+        super().__init__()
+        self.d_model = d_model
+        self.seq_len = seq_len
+        self.dropout = nn.Dropout(dropout)
+
+        pos_encoding = torch.zeros(seq_len, d_model)
+        position = torch.arange(0, seq_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+
+        pos_encoding[:, 0::2] = torch.sin(position * div_term)
+        pos_encoding[:, 1::2] = torch.cos(position * div_term)
+
+        pos_encoding = pos_encoding.unsqueeze(0)
+
+        self.register_buffer('pos_encoding', pos_encoding)
+
+    def forward(self, x):
+        x = x + (self.pos_encoding[:, :x.shape[1], :]).requires_grad_(False)
+        return self.dropout(x)
