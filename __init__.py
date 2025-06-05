c05b08b5a397de14c64eed48ccbcbfddcc15bda7
MrQwest <128bit@128bit.xyz>
2025-06-05 23:29:48 +0300

    refactoring: add pakage module init

---
+++ __init__.py
@@ -0,0 +1,5 @@
+__all__ = ['Tokenizer', 'FileDataset', 'build_transformer', 'Transformer']
+
+from file_dataset import FileDataset
+from tokenizer import Tokenizer
+from transformer_model import build_transformer, Transformer
