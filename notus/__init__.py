c3fd2ca05ca1e932d943d2c75e788726241d490b
MrQwest <128bit@128bit.xyz>
2025-07-24 11:07:50 +0300

    fix: init

--- notus/__init__.py
+++ notus/__init__.py
@@ -1,10 +1,10 @@
-__all__ = ['Tokenizer', 'FileTransformer', 'FileDataset', 'FileTransformerBlock',
-           'ValueTransformerBlock', 'CompressEngine', 'ByteLevelTokenizer',
+__all__ = ['Tokenizer', 'FileTransformerBlock', 'FileDataset', 'FileTransformerBlock',
+           'CompressEngine', 'ByteLevelTokenizer',
            'Encoder', 'Decoder', 'EncoderDecoder']
 
 from notus.file_dataset import FileDataset
 from notus.tokenizer import Tokenizer
-from notus.transformer_model.model  import FileTransformerBlock, ValueTransformerBlock, FileTransformer
+from notus.transformer_model.arch.generator import FileTransformerBlock
 from notus.transformer_model.arch.encoder import EncoderDecoder, Decoder, Encoder
 from notus.engine import CompressEngine
 from notus.tokenizer import ByteLevelTokenizer
