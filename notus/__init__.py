5ba94b8a26e4fd209aa35d1d0b7b52233b40142d
MrQwest <128bit@128bit.xyz>
2025-07-24 11:09:07 +0300

    remove compress Engine

--- notus/__init__.py
+++ notus/__init__.py
@@ -1,10 +1,16 @@
-__all__ = ['Tokenizer', 'FileTransformerBlock', 'FileDataset', 'FileTransformerBlock',
-           'CompressEngine', 'ByteLevelTokenizer',
-           'Encoder', 'Decoder', 'EncoderDecoder']
+__all__ = ['Tokenizer',
+           'FileTransformerBlock',
+           'FileDataset',
+           'FileTransformerBlock',
+           'ByteLevelTokenizer',
+           'Encoder',
+           'Decoder',
+           'EncoderDecoder'
+           ]
 
 from notus.file_dataset import FileDataset
 from notus.tokenizer import Tokenizer
 from notus.transformer_model.arch.generator import FileTransformerBlock
 from notus.transformer_model.arch.encoder import EncoderDecoder, Decoder, Encoder
-from notus.engine import CompressEngine
+#from notus.engine import CompressEngine
 from notus.tokenizer import ByteLevelTokenizer
