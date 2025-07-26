4382952c0ae416436f2e8b2e4c677ca2d36ae26e
MrQwest <128bit@128bit.xyz>
2025-07-26 17:44:23 +0300

    feat: add custom class multihead attn for cross attention encoder

--- notus/transformer_model/arch/generator.py
+++ notus/transformer_model/arch/generator.py
@@ -1,6 +1,46 @@
 import torch
 import torch.nn as nn
 import pytorch_lightning as pl
+import math
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, d_model, n_heads, dropout=0.1):
+        super(MultiHeadAttention, self).__init__()
+        assert d_model % n_heads == 0
+        self.d_model = d_model
+        self.n_heads = n_heads
+        self.dropout = nn.Dropout(dropout)
+        self.d_k = d_model // n_heads
+
+        self.W_q = nn.Linear(d_model, d_model)
+        self.W_k = nn.Linear(d_model, d_model)
+        self.W_v = nn.Linear(d_model, d_model)
+        self.W_o = nn.Linear(d_model, d_model)
+
+    def scaled_dot_product_attention(self, q, k, v, mask=None):
+        batch_size = q.size(0)
+        q = q.view(batch_size, -1, self.n_heads, self.d_k).transpose(1, 2)
+        k = k.view(batch_size, -1, self.n_heads, self.d_k).transpose(1, 2)
+        v = v.view(batch_size, -1, self.n_heads, self.d_k).transpose(1, 2)
+
+        scores = torch.matmul(q, k.transpose(-2, -1)) / math.sqrt(self.d_k)
+
+        if mask is not None:
+            scores = scores.masked_fill(mask == 0, -1e9)
+        attn = torch.softmax(scores, dim=-1)
+        context = torch.matmul(attn, v)
+
+        context = context.transpose(1, 2).contiguous().view(batch_size, -1, self.d_model)
+        return context, attn
+
+    def forward(self, query, key, value, mask=None):
+        Q = self.W_q(query)
+        K = self.W_k(key)
+        V = self.W_v(value)
+
+        context, attn = self.scaled_dot_product_attention(Q, K, V, mask)
+        output = self.W_o(context)
+        return output, attn
 
 class FileTransformerBlock(nn.Module):
     def __init__(self, d_model=1024, nhead=8, num_encoder_layers=6, max_seq_len=4096, embedding_tensor=1024, vocab_size=267):
