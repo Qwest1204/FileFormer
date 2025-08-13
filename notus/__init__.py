312171e7f267c6d3a2b1661e8838bf757c83118b
MrQwest <128bit@128bit.xyz>
2025-08-13 17:50:01 +0300

    feat: using GRU to encode hash

--- notus/__init__.py
+++ notus/__init__.py
@@ -1,16 +1,11 @@
 __all__ = ['Tokenizer',
-           'FileTransformerBlock',
            'FileDataset',
-           'FileTransformerBlock',
            'ByteLevelTokenizer',
-           'Encoder',
-           'Decoder',
-           'EncoderDecoder'
+           'EncoderGRU',
            ]
 
 from notus.file_dataset import FileDataset
 from notus.tokenizer import Tokenizer
-from notus.transformer_model.arch.generator import FileTransformerBlock
-from notus.transformer_model.arch.encoder import EncoderDecoder, Decoder, Encoder
+from notus.transformer_model.arch.encoder import EncoderGRU
 #from notus.engine import CompressEngine
 from notus.tokenizer import ByteLevelTokenizer
