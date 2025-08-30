e75837ef2a0604596d40b0045aca0485d67bc782
MrQwest <128bit@128bit.xyz>
2025-08-30 14:11:20 +0300

    update initial lincs for modules

--- notus/__init__.py
+++ notus/__init__.py
@@ -1,11 +1,18 @@
 __all__ = ['Tokenizer',
            'FileDataset',
            'ByteLevelTokenizer',
-           'EncoderGRU',
+           'Encoder',
+           'attention',
+           'mlp',
+           'utils',
            ]
 
 from notus.file_dataset import FileDataset
 from notus.tokenizer import Tokenizer
-from notus.transformer_model.arch.encoder import EncoderGRU
+from notus.transformer_model.arch.encoder import Encoder
+#from notus.transformer_model.arch.attention import SelfAttention, MultiHeadAttention, MultiQueryAttention, MultiHeadLatentAttention
 #from notus.engine import CompressEngine
 from notus.tokenizer import ByteLevelTokenizer
+import notus.transformer_model.arch.attention as attention
+import notus.transformer_model.arch.mlp as mlp
+import notus.transformer_model.utils as utils
