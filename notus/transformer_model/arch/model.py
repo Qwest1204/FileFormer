bb8bc8a31cc0be93b55d239803f549ecb24b1376
Qwest1204 <myPCwithUbuntu@gmail.com>
2025-10-30 15:47:29 +0300

    fixing model to first working model (after train)

--- notus/transformer_model/arch/model.py
+++ notus/transformer_model/arch/model.py
@@ -14,14 +14,14 @@ class FileFormer(L.LightningModule):
         self.create_models()
 
     def create_models(self):
-        self.encoder = Encoder(**self.config['encoder']).to(self.config['train']['device'])
-        self.decoder = Decoder(**self.config['decoder']).to(self.config['train']['device'])
+        self.encoder = Encoder(**self.config['encoder'])
+        self.decoder = Decoder(**self.config['decoder'])
 
     def training_step(self, batch, batch_idx):
-        metadata, tokens, masked_tokens, hash, extention_tokenize = batch
-        encoder_out = self.encoder(metadata, hash, extention_tokenize)
+        tokens, masked_tokens, pads, hash, extention_tokenize = batch
+        encoder_out = self.encoder(hash, extention_tokenize)
 
-        decoder_out = self.decoder(masked_tokens, encoder_out)
+        decoder_out = self.decoder(masked_tokens, encoder_out, pads)
 
         loss = self.loss_fn(
             decoder_out.view(-1, self.config['encoder']['vocab_size']),
@@ -35,7 +35,7 @@ class FileFormer(L.LightningModule):
 
         opt.step()
 
-        if batch_idx % self.config['train']['interval4save'] == 0:
+        if batch_idx % self.configs['train']['interval4save'] == 0:
             self.eval.evaluate(self.forward, batch)
         return loss
 
@@ -49,7 +49,7 @@ class FileFormer(L.LightningModule):
             "optimizer": optimizer,
         }
 
-    def forward(self, meta, data, hash, file_extention, pads=None):
-        encoder_out = self.encoder(meta, hash, file_extention)
+    def forward(self, data, hash, file_extention, pads=None):
+        encoder_out = self.encoder(hash, file_extention)
         decoder_out = self.decoder(data, encoder_out, pads)
         return decoder_out
