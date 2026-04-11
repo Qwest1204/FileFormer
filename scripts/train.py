49dfc85ca4658da4add5fdc3264b64466639eecf
Qwest1204 <myPCwithUbuntu@gmail.com>
2026-04-11 14:55:50 +0300

    refactoring: fix name mistake trainig -> training write final fuction to train model
    Signed-off-by: Qwest1204 <myPCwithUbuntu@gmail.com>

--- scripts/train.py
+++ scripts/train.py
@@ -1,128 +1,96 @@
-
 import os
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
-from tqdm import tqdm
 
-from fileformer import FileFormer
-from fileformer.file_dataset import ENWIK8Dataset
+from model import FileFormer, ByteLevelTokenizer
+from dataset import FileDataset
+from training import train_one_epoch
 
+tokenizer = ByteLevelTokenizer()
 
-def train_fileformer(
-        model: FileFormer,
-        train_dataset,
-        epochs: int = 50,
-        batch_size: int = 32,
-        lr: float = 3e-4,
-        device: str = "cuda",
-        save_dir: str = "checkpoints",
-):
-    os.makedirs(save_dir, exist_ok=True)
-    print(1)
+VOCAB_SIZE = tokenizer.vocab_size
+EMBED_SIZE = 256
+SEQ_LEN = 8096
+VAL_LEN = int(SEQ_LEN/2)
+N_HEADS = 4
+N_LAYERS = 6
+DROP_RATE = 0.07
+BATCH_SIZE = 32
+EPOCHS = 10
+LR = 3e-5
+CLIP_GRAD_NORM = 10.0
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+SAVE_DIR = "../checkpoints/"
 
-    model = model.to(device)
-    print(1)
+train_dataset = FileDataset(file_path="../data/raw/<file>", seq_len=SEQ_LEN, mask_prob=DROP_RATE, cache_dir="../data/.cache")
+val_dataset = FileDataset(file_path="../data/raw/<file>", seq_len=VAL_LEN, mask_prob=DROP_RATE, cache_dir="../data/.cache")
 
-    train_loader = DataLoader(
-        train_dataset,
-        batch_size=batch_size,
-        shuffle=True,
-        num_workers=0,
-        pin_memory=True
-    )
-    print(1)
+train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=0)
+val_loader = DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=0)
 
-    optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
-    criterion = nn.CrossEntropyLoss(ignore_index=256)  # игнорируем pad_token_id
-    print(1)
-
-    for epoch in range(1, epochs + 1):
-        # ===== Training =====
-        model.train()
-        train_loss = 0.0
-
-        pbar = tqdm(train_loader, desc=f"Epoch {epoch}/{epochs} [Train]")
-        for input_ids, padding_mask, _ in pbar:
-            input_ids = input_ids.to(device)
-            padding_mask = padding_mask.to(device)
-
-            x = input_ids[:, :-1]
-            y = input_ids[:, 1:]
-
-            pad_mask = padding_mask[:, 1:]
-
-            # Forward
-            logits = model(x, pad_mask)  # [batch, seq_len-1, vocab_size]
-
-            # Reshape для loss
-            logits = logits.reshape(-1, logits.size(-1))  # [batch * (seq_len-1), vocab_size]
-            y = y.reshape(-1)  # [batch * (seq_len-1)]
-
-            loss = criterion(logits, y)
-
-            # Backward
-            optimizer.zero_grad()
-            loss.backward()
-            torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
-            optimizer.step()
-
-            train_loss += loss.item()
-            pbar.set_postfix({"loss": f"{loss.item():.4f}"})
-
-        avg_train_loss = train_loss / len(train_loader)
-        # ===== Logging =====
-        log_msg = f"Epoch {epoch}: train_loss={avg_train_loss:.4f}"
-        print(log_msg)
-
-        # ===== Save checkpoint =====
-        checkpoint = {
-            "epoch": epoch,
-            "model_state_dict": model.state_dict(),
-            "optimizer_state_dict": optimizer.state_dict(),
-            "train_loss": avg_train_loss,
-        }
-        checkpoint_path = os.path.join(save_dir, f"model_epoch_{epoch}.pt")
-        torch.save(checkpoint, checkpoint_path)
-        print(f"Saved checkpoint: {checkpoint_path}")
-
-    return model
-
-
-# ===== Пример использования =====
-if __name__ == "__main__":
-    VOCAB_SIZE = 257
-    EMBED_SIZE = 256
-    MAX_SEQ_LEN = 16828
-    N_HEADS = 4
-    N_LAYERS = 6
-    DROP_RATE = 0.0
-    print(1)
-    # Датасет
-    train_dataset = ENWIK8Dataset(
-        file_path="/Users/daniilogorodnikov/PycharmProjects/Notus/enwik8",
-        seq_len=MAX_SEQ_LEN,
-        overlap=0,
-        cache_dir="/Users/daniilogorodnikov/PycharmProjects/Notus/cache"
-    )
-    print(1)
-    # Модель
-    model = FileFormer(
+model = FileFormer(
         vocab_size=VOCAB_SIZE,
         embed_size=EMBED_SIZE,
-        max_seq_len=MAX_SEQ_LEN,
         n_heads=N_HEADS,
         n_layers=N_LAYERS,
         drop_rate=DROP_RATE
     )
 
-    # Обучение
-    train_fileformer(
-        model=model,
-        train_dataset=train_dataset,
-        epochs=10,
-        batch_size=2,
-        lr=3e-4,
-        device="cuda" if torch.cuda.is_available() else "cpu",
-        save_dir="checkpoints"
+optimizer = torch.optim.AdamW(model.parameters(), lr=LR)
+loss_fn = nn.CrossEntropyLoss(ignore_index=1)  # игнорируем pad_token_id
+
+def main(
+        model,
+        optimizer,
+        loss_fn,
+        train_dataloader,
+        val_dataloader,
+        device,
+        epochs,
+        clip_grad_norm,
+        save_dir,
+    ):
+
+    for epoch in range(1, epochs + 1):
+
+        model, optimizer, avg_train_loss, avg_val_loss = train_one_epoch(
+            model=model,
+            optimizer=optimizer,
+            loss_fn=loss_fn,
+            train_dataloader=train_dataloader,
+            val_dataloader=val_dataloader,
+            device=device,
+            epoch=epoch,
+            clip_grad_norm=clip_grad_norm,
+            )
+
+        checkpoint = {
+            "model_state_dict": model.state_dict(),
+            "optimizer_state_dict": optimizer.state_dict(),
+            "config": {
+                "vocab_size": VOCAB_SIZE,
+                "embed_size": EMBED_SIZE,
+                "n_heads": N_HEADS,
+                "n_layers": N_LAYERS,
+                "drop_rate": DROP_RATE
+            }
+        }
+        checkpoint_path = os.path.join(save_dir, f"model_epoch_{epoch}.pt")
+        torch.save(checkpoint, checkpoint_path)
+        print(f"Saved checkpoint: {checkpoint_path}")
+
+    return 0
+
+
+if __name__ == "__main__":
+    main(model,
+        optimizer,
+        loss_fn,
+        train_loader,
+        val_loader,
+        DEVICE,
+        EPOCHS,
+        CLIP_GRAD_NORM,
+        SAVE_DIR
     )
